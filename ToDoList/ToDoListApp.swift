40a5ad58277aad36b31ec2ac71ffad9ead30d864
JohnMP <j_ponce3@u.pacific.edu>
2025-03-22 09:51:39 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by John Milton Ponce on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
